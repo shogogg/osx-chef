38c15b9397309b98b7476d535105e95ca8cf757f
shogo <shogo@studiofly.net>
2013-10-25 07:44:53 +0900

    Add cookbook osx-apps

---
+++ site-cookbooks/osx-apps/README.md
@@ -0,0 +1,3 @@
+osx-apps
+=============
+Recipes to install commonly used applications on OS X Mavericks.
---
+++ site-cookbooks/osx-apps/metadata.rb
@@ -0,0 +1,9 @@
+name             'osx-apps'
+maintainer       'Shogo Kawase'
+maintainer_email 'shogo@studiofly.net'
+license          'MIT'
+description      'Installs/Configures osx-apps'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+supports         'mac_os_x'
+depends          'dmg'
